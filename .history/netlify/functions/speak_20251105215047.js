848a0385f131da917435e33cbd7a3ec699f70db1
Shivam5571 <shivamverma200423@gmail.com>
2025-11-05 21:52:17 +0530

    Switch to pdf-parse-fixed to fix DOMMatrix error

--- .history/netlify/functions/speak_20251105215047.js
+++ .history/netlify/functions/speak_20251105215047.js
@@ -14,7 +14,7 @@ export async function handler(event) {
     }
 
     // 👉 Replace this ID with your own custom voice ID
-    const voiceId = "1qEiC6qsybMkmnNdVMbK"; // <-- your custom ID here
+    const voiceId = "pMsXgVXv3BLzUgSXRplE"; // <-- your custom ID here
 
     const response = await fetch(
       `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`,
